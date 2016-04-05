57a40cd4a987d165841d0a999ee3e936c5756fc8
Matt K <mjkerster@gmail.com>
2016-04-04 21:09:57 -0400

    more style changes

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ var domHelper = function(){
 			}
 			//template = '<div><img src="'+imageSrc+'">'+listData[i].nameDisplay+'</div>';><span>ABV: '+listData[i].abv+'%</span><span>'+listData[i].style.name+'</span></div><div class="beer-description pure-u-1">'+listData[i].description+'</div></div></li>';
 			
-			template = '<li class="beer-row pure-u-1"><div class="pure-u-1"><div class="beer-image pure-u-1-8"><img src="'+imageSrc+'"></div><div class="title-section pure-u-4-5"><span class="title pure-u-1">'+listData[i].nameDisplay+'</span><span>'+listData[i].style.name+' </span><span>ABV: '+listData[i].abv+'%</span></div></div><div class="beer-description pure-u-1"><p>'+listData[i].description+'</p></div></li>'
+			template = '<li class="beer-row pure-u-1"><div class="pure-u-1"><div class="beer-image pure-u-1-8"><img src="'+imageSrc+'"></div><div class="title-section pure-u-5-8"><span class="title pure-u-1">'+listData[i].nameDisplay+'</span><span class="pure-u-1 style">'+listData[i].style.name+' </span><span class="pure-u-1 abv">ABV: '+listData[i].abv+'%</span></div></div><div class="beer-description pure-u-1"><p>'+listData[i].description+'</p></div></li>'
 
 			el.insertAdjacentHTML('beforeend', template);
 		}
